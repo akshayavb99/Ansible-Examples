4d58ab7334f5f321f6aa4d05af4bde82c139e12d
Akshaya Balaji <akshayavb99@gmail.com>
2021-01-24 16:05:36 +0530

    setup for haproxy

---
+++ setup-haproxy/roles/webserver_role/files/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1> Welcome to the Test Page  </h1>
+<h2> Page created by Akshaya Balaji. <h2>
+<pre>
+<?php 
+print `/usr/sbin/ifconfig`; 
+?>
+</pre>
+</body>
+</html>
